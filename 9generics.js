51f6565d809b0c1ee2818d3cdb096d95f576d09f
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-02-25 14:57:40 +0530

    Add files via upload

---
+++ 9generics.js
@@ -0,0 +1,9 @@
+//generics allows us to be flexible regarding the time certain objects use
+//array is a generic class available for almost all the data types
+//but we can limit it to runtime saying that in the specific area this variable is limited only to use numbers
+// Generics are types which can hold/ use several types
+// Consider the Array object
+var numberArray; // This array will only accept numbers
+// Try to initialize it with strings
+// numberArray = ['test']; // => Error
+numberArray = [1, 2, 3];
